a4ba19a1924e727679b0fa0d1ce55a0d117eb75c
Rodolfo Nei <rodolfonei.webdev@gmail.com>
2026-06-18 14:15:36 -0300

    inventory redis config and models

--- inventory/main.py
+++ inventory/main.py
@@ -1,7 +1,23 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+from redis_om import get_redis_connection, HashModel
+
+load_dotenv()
 
 app = FastAPI()
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+redis = get_redis_connection(
+    host=os.getenv("HOST"),
+    port=os.getenv("PORT")
+    password=os.getenv("PASSWORD")
+    decode_responses=True
+)
+
+class Product(HashModel):
+    name: str
+    price: float
+    quantity: int
+
+    class Meta:
+        database = redis
+
