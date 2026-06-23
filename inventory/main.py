baceded8aac212e7f51a43fef1c17c35b13c65c3
Rodolfo Nei <rodolfonei.webdev@gmail.com>
2026-06-23 18:10:20 -0300

    add middleware and first api endpoints

--- inventory/main.py
+++ inventory/main.py
@@ -1,4 +1,6 @@
+import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from dotenv import load_dotenv
 from redis_om import get_redis_connection, HashModel
 
@@ -6,14 +8,21 @@ load_dotenv()
 
 app = FastAPI()
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 redis = get_redis_connection(
     host=os.getenv("HOST"),
-    port=os.getenv("PORT")
-    password=os.getenv("PASSWORD")
+    port=os.getenv("PORT"),
+    password=os.getenv("PASSWORD"),
     decode_responses=True
 )
 
-class Product(HashModel):
+class Product(HashModel, index=True):
     name: str
     price: float
     quantity: int
@@ -21,3 +30,10 @@ class Product(HashModel):
     class Meta:
         database = redis
 
+@app.get("/products")
+def all():
+    return Product.all_pks()
+
+@app.post("/products")
+def create(product: Product):
+    return product.save()
