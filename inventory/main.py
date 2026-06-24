c3be2d0fa7800d30245ab13f63143306dac71c6e
Rodolfo Nei <rodolfonei.webdev@gmail.com>
2026-06-24 09:18:37 -0300

    add Delete and Get a single product endpoints

--- inventory/main.py
+++ inventory/main.py
@@ -30,10 +30,28 @@ class Product(HashModel, index=True):
     class Meta:
         database = redis
 
+def format(pk: str):
+    product = Product.get(pk)
+
+    return {
+        "id": product.pk,
+        "name": product.name,
+        "price": product.price,
+        "quantity": product.quantity
+    }
+
 @app.get("/products")
 def all():
-    return Product.all_pks()
+    return [format(pk) for pk in Product.all_pks()]
 
 @app.post("/products")
 def create(product: Product):
     return product.save()
+
+@app.get("/products/{pk}")
+def get(pk: str):
+    return Product.get(pk)
+
+@app.delete("/products/{pk}")
+def delete(pk: str):
+    return Product.delete(pk)
